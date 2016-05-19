7645c7065b322297a756efc440b5229766f190dd
VesterDe <demjan.vester@gmail.com>
2016-05-19 16:12:20 +0200

    Removed debugging functions and generic function names.

--- MCE_resolver.php
+++ MCE_resolver.php
@@ -1,35 +1,30 @@
 <?php
 /*
 Plugin Name: MCE_reseter
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Plugin URI: https://github.com/VesterDe/wp_mce_wplink_quickfix/
+Description: This plugin removes the inline link editing in tinyMCE. Also, it sends users to hell. You have been warned.
 Author: Demjan Vester
-Version: 0.1
+Version: 0.0.1
 */
-add_filter( 'mce_buttons', 'myplugin_register_buttons' );
+add_filter( 'mce_buttons', 'add_fixed_mce_plugin' );
 
-function myplugin_register_buttons( $buttons ) {
-   write_log($buttons);
+function add_fixed_mce_plugin( $buttons ) {
    array_push( $buttons, 'separator', 'wplinkc' );
    return $buttons;
 }
 
-// Load the TinyMCE plugin : editor_plugin.js (wp2.5)
-add_filter( 'mce_external_plugins', 'myplugin_register_tinymce_javascript' );
+add_filter( 'mce_external_plugins', 'add_fixed_mce_js' );
 
-function myplugin_register_tinymce_javascript( $plugin_array ) {
-   write_log($plugin_array);
+function add_fixed_mce_js( $plugin_array ) {
    $plugin_array['wplinkc'] = plugins_url( '/js/plugin.js',__FILE__ );
    return $plugin_array;
 }
 function disable_mce_wptextpattern( $opt ) {
-   write_log($opt);
     if ( isset( $opt['plugins'] ) && $opt['plugins'] ) {
         $opt['plugins'] = explode( ',', $opt['plugins'] );
         $opt['plugins'] = array_diff( $opt['plugins'] , array( 'wplink' ) );
         $opt['plugins'] = implode( ',', $opt['plugins'] );
     }
-
     return $opt;
 }
 
